0f419998130512ded460b62cac167fd6d0f8160e
rajswd <raj.swd1@gmail.com>
2022-05-08 12:46:12 +0800

    1 file modified

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MyService } from '../services/my.service';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.css']
+  styleUrls: ['./display.component.css'],
 })
 export class DisplayComponent implements OnInit, OnDestroy {
-
   ngUnsubscribe: any = new Subject();
   description: string;
   dataList: any[] = [];
   constructor(private myService: MyService) {}
 
   ngOnInit() {
-    this.myService.data$.asObservable().subscribe((data: any) => {
-      if (data && data.dataList) {
-        this.dataList = data.dataList || [];
-        this.description = data.description;
-      }
-    });
+    this.myService.data$
+      .asObservable()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((data: any) => {
+        if (data && data.dataList) {
+          this.dataList = data.dataList || [];
+          this.description = data.description;
+        }
+      });
   }
 
   ngOnDestroy() {
